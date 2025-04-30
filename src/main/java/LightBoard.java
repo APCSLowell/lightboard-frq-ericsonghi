96e785e1c2d598a132bcdc0b89bb1e3403327e8b
Eric Song <146142807+ericsonghi@users.noreply.github.com>
2025-04-30 00:36:35 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -47,10 +47,9 @@ public boolean evaluateLight(int row, int col){
     }
       if(isV%3 == 0){
         return true;
-      }else{
-      return lights[row][col];
       }
   }  
+  return lights[row][col];
 }
   
   public boolean[][] getLights()
