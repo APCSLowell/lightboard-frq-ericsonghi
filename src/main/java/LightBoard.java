396f06662dfd477e858cba9bde23b756dda75fd6
Eric Song <146142807+ericsonghi@users.noreply.github.com>
2025-04-30 00:25:06 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -12,7 +12,7 @@ public class LightBoard
   {
     lights = new boolean [numRows][numCols];
     for(int i = 0; i < numRows; i++){
-        for(int j = 0; j M numCols; j++){
+        for(int j = 0; j < numCols; j++){
             if(Math.random() > 0.4){
                 lights[i][j] = false;
             else{
